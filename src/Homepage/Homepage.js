b4d28b88be768385f2e07d907ce8a64c48602c03
pmas98 <pmas98@hotmail.com>
2024-05-21 16:01:55 -0300

    chore: Update userChat component styling and audio playback functionality

--- src/Homepage/Homepage.js
+++ src/Homepage/Homepage.js
@@ -1,16 +1,16 @@
 // Home.js
 import React, { useEffect } from "react";
 import Navbar from "../components/navbar";
-import Carousel from "../components/carousel"
+import Carousel from "../components/carousel";
 import { useState } from "react";
 
-const Homepage = () => {  
+const Homepage = () => {
   return (
     <div>
       <Navbar />
       <div className="px-4 md:px-24 sm:px-16 md:py-16 sm:py-16 py-6 flex flex-col lg:flex-row items-center">
-        <div className="md:flex-1 sm:flex-1 px-12">
-          <h1 className="xl:text-8xl md:text-7xl sm:text-6xl text-6xl font-primaryRegular text-gray-500 mb-4 md:mb-8 sm:mb-8">
+        <div className="md:flex-1 sm:flex-1 px-12 sm:pr-0">
+          <h1 className="xl:text-8xl md:text-7xl sm:text-6xl text-5xl font-primaryRegular text-gray-500 mb-4 md:mb-8 sm:mb-8">
             Sua plataforma de saúde mental{" "}
             <span className="text-[#e21b5a] font-primaryBold underline">
               colaborativa
@@ -21,11 +21,11 @@ const Homepage = () => {
             ajudar outras pessoas.
           </h3>
         </div>
-        <div className="md:flex-1 sm:flex-1">
+        <div className="md:flex-1 sm:flex-1 mt-8 md:mt-0">
           <img
             src="people.jpg"
             alt="Image"
-            className="rounded-md border xl:w-full sm:w-full md:w-auto mx-auto md:mx-0"
+            className="rounded-md border w-full object-cover object-center mx-auto"
           />
         </div>
       </div>
@@ -40,31 +40,33 @@ const Homepage = () => {
       </div>
 
       <hr className="w-[40vw] border-[#e21b5a] mx-auto border-[.1vw] mb-4" />
-      <div className="py-8 mx-12 lg:mx-24">
+      <div className="py-8 mx-4 lg:mx-24">
         <div className="bg-[#e21b5a] rounded-lg border border-gray-200 shadow-md p-8 max-w-6xl mx-auto sm:max-w-full sm:px-4">
           <h2 className="font-primaryBold text-2xl sm:text-4xl font-bold text-center text-white">
             Como Funciona
           </h2>
         </div>
-        <div className="flex flex-col md:flex-row items-center pt-8 pb-12">
+        <div className="flex flex-col md:flex-row items-center pt-8 pb-12 px-6">
           <div className="md:w-1/2 mb-8 md:mb-0 md:mr-8 order-2 md:order-1">
             <img
-              src="https://placehold.co/800x400"
+              src="example.png"
               alt="Image"
-              className="rounded-md border w-full"
+              className="rounded-md border w-full shadow-lg transition-all duration-300 hover:shadow-xl"
             />
           </div>
 
-          <div className="md:w-2/3 order-2 md:order-1 lg:ml-16">
-            <h3 className="font-primaryBold text-2xl md:text-4xl lg:text-6xl xl:text-8xl font-bold mb-2">
-              BIG Text with Underline
+          <div className="md:w-2/3 order-1 md:order-2 lg:ml-16">
+            <h3 className="font-primaryBold text-4xl md:text-4xl lg:text-4xl xl:text-6xl font-bold mb-2">
+              Um espaço seguro de apoio.
             </h3>
-            <p className="font-primaryMedium text-sm md:text-base text-gray-500">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
-              type and scrambled it to make a type specimen book.
+            <p className="font-primaryMedium text-base md:text-lg text-gray-500">
+              O mindsupport oferece um ambiente seguro e acolhedor para você se conectar com outras pessoas que estão passando por desafios semelhantes.
             </p>
+              
+            <p className="font-primaryMedium text-base md:text-lg text-gray-500">
+            Em sessões moderadas você poderá compartilhar suas experiências, buscar ajuda e ajudar outras pessoas.
+            </p>
+
           </div>
         </div>
         <div className="bg-[#e21b5a] rounded-lg border border-gray-200 shadow-md p-8 max-w-6xl mx-auto sm:max-w-full sm:px-4">
@@ -72,24 +74,24 @@ const Homepage = () => {
             Nossos Valores
           </h2>
         </div>
-        <div className="flex flex-col md:flex-row justify-center items-center md:space-x-24 xl:space-x-64 my-16">
+        <div className="flex flex-col md:flex-row justify-center items-center md:space-x-8 xl:space-x-24 my-16">
           <div className="flex flex-col items-center mb-8 md:mb-0">
-            <div className="w-40 h-40 bg-[#e21b5a] rounded-full flex justify-center items-center mb-4"></div>
-            <p className="font-primaryMedium text-2xl text-center text-gray-600">
+            <div className="w-32 h-32 md:w-40 md:h-40 bg-[#e21b5a] rounded-full flex justify-center items-center mb-4"></div>
+            <p className="font-primaryMedium text-lg md:text-2xl text-center text-gray-600">
               Segurança
             </p>
           </div>
 
           <div className="flex flex-col items-center mb-8 md:mb-0">
-            <div className="w-40 h-40 bg-[#e21b5a] rounded-full flex justify-center items-center mb-4"></div>
-            <p className="font-primaryMedium text-2xl text-center text-gray-600">
+            <div className="w-32 h-32 md:w-40 md:h-40 bg-[#e21b5a] rounded-full flex justify-center items-center mb-4"></div>
+            <p className="font-primaryMedium text-lg md:text-2xl text-center text-gray-600">
               Privacidade
             </p>
           </div>
 
           <div className="flex flex-col items-center">
-            <div className="w-40 h-40 bg-[#e21b5a] rounded-full flex justify-center items-center mb-4"></div>
-            <p className="font-primaryMedium text-2xl text-center text-gray-600">
+            <div className="w-32 h-32 md:w-40 md:h-40 bg-[#e21b5a] rounded-full flex justify-center items-center mb-4"></div>
+            <p className="font-primaryMedium text-lg md:text-2xl text-center text-gray-600">
               Suporte
             </p>
           </div>
@@ -97,7 +99,7 @@ const Homepage = () => {
       </div>
       <footer className="bg-[#e21b5a] py-8">
         <div className="container mx-auto text-center">
-          <p className="font-primaryMedium text-white text-2xl">
+          <p className="font-primaryMedium text-white text-xl md:text-2xl">
             Todos os direitos reservados
           </p>
         </div>
--- src/components/userChat.js
+++ src/components/userChat.js
@@ -53,59 +53,54 @@ const UserChat = ({
         />
       ) : null}
       <div
-        className={`mt-3 w-10 h-10 rounded-full ${color} ${
+        className={`ml-4 mt-3 w-10 h-10 rounded-full ${color} ${
           isOwnMessage ? "ml-2 sm:ml-4" : "mr-2 sm:mr-4"
         } ${
-          isModerator && isOwnMessage
+          isModerator
             ? "outline outline-green-500 outline-4"
             : ""
         }`}
       ></div>
-      <div
-        className={`mt-2 flex-1 ${
-          isAudioMessage(audio)
-            ? "max-w-[200px]"
-            : "max-w-[200px] sm:max-w-[200px] md:max-w-[200px]"
-        } ${
-          isOwnMessage
-            ? "bg-gray-200 border border-gray-200 rounded-md text-right px-2 py-1 sm:px-4 sm:py-2"
-            : ""
-        }`}
-      >
-        <div
-          className={`flex items-center ${
-            isOwnMessage ? "justify-end" : "justify-between"
-          }`}
+<div
+  className={`mt-2 flex-1 ${
+    isAudioMessage(audio)
+      ? "max-w-[200px]"
+      : isOwnMessage
+      ? "bg-gray-200 border border-gray-200 rounded-md text-right px-2 py-1 sm:px-4 sm:py-2"
+      : ""
+  } ${isOwnMessage ? "bg-gray-200 border border-gray-200 rounded-md text-right px-2 py-1 sm:px-4 sm:py-2" : ""}`}
+>
+  <div
+    className={`flex items-center ${
+      isOwnMessage ? "justify-end" : "justify-between"
+    }`}
+  >
+    <h3 className="font-primaryBold text-sm sm:text-base">{username}</h3>
+  </div>
+
+  <div
+    className={`font-primaryRegular text-gray-700 break-words pr-2 sm:pr-3 ${
+      isOwnMessage ? "ml-auto" : ""
+    }`}
+  >
+    {isAudioMessage(audio) ? (
+      <div className="flex pl-2 sm:pl-0">
+        <button
+          onClick={handleAudioPlayPause}
+          className="bg-blue-500 text-white px-2 py-1 sm:px-4 sm:py-2 rounded-full flex items-center"
         >
-          <h3 className="font-primaryBold text-sm sm:text-base">{username}</h3>
-        </div>
-        <div
-          className={` font-primaryRegular text-gray-700 break-words pr-2 sm:pr-3 ${
-            isOwnMessage ? "ml-auto" : ""
-          }`}
-        >
-          {isAudioMessage(audio) ? (
-            <div className="flex pl-2 sm:pl-0">
-              <button
-                onClick={handleAudioPlayPause}
-                className="bg-blue-500 text-white px-2 py-1 sm:px-4 sm:py-2 rounded-full flex items-center"
-              >
-                <i className={`fas ${isPlaying ? "fa-pause" : "fa-play"}`}></i>
-                <span className="ml-2 hidden sm:inline">
-                  {isPlaying ? "Pause" : "Play"} Audio
-                </span>
-              </button>
-              <audio
-                ref={audioRef}
-                src={audio}
-                onEnded={() => setIsPlaying(false)}
-              />
-            </div>
-          ) : (
-            <p className="text-sm sm:text-base">{message}</p>
-          )}
-        </div>
+          <i className={`fas ${isPlaying ? "fa-pause" : "fa-play"}`}></i>
+          <span className="ml-2 hidden sm:inline">
+            {isPlaying ? "Pause" : "Play"} Audio
+          </span>
+        </button>
+        <audio ref={audioRef} src={audio} onEnded={() => setIsPlaying(false)} />
       </div>
+    ) : (
+      <p className="text-sm sm:text-base">{message}</p>
+    )} 
+  </div>
+</div>
       <div className="mt-3 flex items-center">
         {userIsModerator && !isOwnMessage && (
           <button onClick={handleModal} className="font-primaryBold w-8 h-8 sm:w-10 sm:h-10 rounded-full bg-red-500 text-white flex items-center justify-center">
