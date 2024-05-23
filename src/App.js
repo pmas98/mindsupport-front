e7f450df89dca0530b554a4c9d78a4482b8b59df
davibonitto <134442652+davibonitto@users.noreply.github.com>
2024-05-23 11:07:34 -0300

    Update App.js
    adicionei a tela de login e de registro aqui no app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Homepage from './Homepage/Homepage';
 import Chat from './Chat/Chat';
 import Profile from './Profile/Profile';
+import Login from './Login/Login';
+import Registro from './Registro/Registro'
 
 const App = () => {
   return (
@@ -12,6 +14,8 @@ const App = () => {
           <Route exact path="/" element={<Homepage />} />
           <Route exact path="/chat" element={<Chat />} />
           <Route exact path="/profile" element={<Profile />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/registro" element={<Registro />} />
         </Routes>
       </div>
     </Router>
